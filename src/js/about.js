26e259b4c95888ea9b9045aaace594cd1b92b3af
Andrey389 <148715628+Andrey389@users.noreply.github.com>
2024-04-14 22:11:28 +0300

    add JS and HTML and CSS about me

--- src/js/about.js
+++ src/js/about.js
@@ -1,71 +1,72 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper';
-import { Navigation, Keyboard } from 'swiper/modules';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 import 'swiper/css';
 
-
-const swiperBoxAbout = document.querySelector('.about-swiper');
-const nextBtn = document.querySelector('.about-swiper-button-next');
-
-
-
-new Swiper(swiperBoxAbout, {
-    loop: true,
-    modules: [Navigation, Keyboard],
-    slidesPerView: 2,
-  navigation: {
-    nextEl: nextBtn,
-    },
-    keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+new Swiper('.about-swiper', {
+  loop: true,
   direction: 'horizontal',
-  centeredSlides: true,
-  watchOverflow: false,
+  allowSlideNext: true,
+  setWrapperSize: true,
+  modules: [Navigation, Keyboard, Mousewheel],
+  simulateTouch: true,
+  grabCursor: true,
+  spaceBetween: 0,
+  speed: 1000,
+  navigation: {
+    nextEl: '.about-swiper-button-next',
+  },
   breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    375: {
+      slidesPerView: 2,
+    },
     768: {
       slidesPerView: 3,
     },
-
     1440: {
-        slidesPerView: 6,
+      slidesPerView: 6,
     },
   },
-  disabledClass: 'swiper-button-nest-disabled',
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    invert: true,
+  },
 });
 
-
-
-
-    new Accordion('.about-education', { duration: 600,
+new Accordion('.about-education', {
+  duration: 600,
   collapse: true,
   openOnInit: [0],
   showMultiple: true,
   onOpen: () => {
-    aboutArr.classList.add("rotate");
+    aboutArr.classList.add('rotate');
   },
   onClose: () => {
-    aboutArr.classList.remove("rotate");
-  }
+    aboutArr.classList.remove('rotate');
+  },
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const accordions = document.querySelectorAll('.about-education-list');
+
+  accordions.forEach(accordion => {
+    accordion.addEventListener('click', function () {
+      const arrow = this.querySelector('.about-arrow');
+      const description = this.querySelector('.text-acordion');
+
+      arrow.classList.toggle('rotate');
+      description.classList.toggle('visually-hidden');
     });
+  });
 
-    document.addEventListener("DOMContentLoaded", function() {
-    const accordions = document.querySelectorAll('.about-education-list');
-
-    accordions.forEach(accordion => {
-        accordion.addEventListener('click', function() {
-            const arrow = this.querySelector('.about-arrow');
-            const description = this.querySelector('.text-acordion')
-
-            arrow.classList.toggle('rotate');
-            description.classList.toggle('visually-hidden');
-        });
-    });
-
-    new Accordion('.faq-section', {
-        duration: 400,
-        showFirstItem: false,
-    });
+  new Accordion('.faq-section', {
+    duration: 400,
+    showFirstItem: false,
+  });
 });
