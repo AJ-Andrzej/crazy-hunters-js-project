792aa8bdc6453e216de061f8995c8085f6a92c07
Barbiett <146207003+Barbiett@users.noreply.github.com>
2024-04-15 17:46:19 +0300

    Закончил.

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,19 +1,17 @@
-
+// swiperProjects.on('slideChange', function () {
+//   nextButton.disabled = swiperProjects.isEnd ? true : false;
+//   prevButton.disabled = swiperProjects.isBeginning ? true : false;
+// });
+// ============================================================== Ментор.
 import Swiper from 'swiper';
 import { Navigation, Keyboard } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 Swiper.use([Navigation, Keyboard]);
 
-
+const swiperBox = document.querySelector('.swiper-project');
 export const nextButton = document.querySelector('.projects-next-btn');
 export const prevButton = document.querySelector('.projects-prev-btn');
-
-const swiperProjects = new Swiper('.swiper-project', {
-  preventInteractionOnTransition: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
 
 prevButton.addEventListener('click', function () {
   swiperProjects.slidePrev();
@@ -23,61 +21,57 @@ nextButton.addEventListener('click', function () {
   swiperProjects.slideNext();
 });
 
-swiperProjects.on('slideChange', function () {
-  nextButton.disabled = swiperProjects.isEnd ? true : false;
-  prevButton.disabled = swiperProjects.isBeginning ? true : false;
+
+const swiperProjects = new Swiper(swiperBox, {
+  preventInteractionOnTransition: true,
+  navigation: {
+    nextEl: nextButton,
+    prevEl: prevButton,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
 });
 
 
-// function updateButtonState() {
-//   if (swiperProjects.isEnd) {
-//     nextButton.classList.add('disabled-button');
-//   } else {
-//     nextButton.classList.remove('disabled-button');
-//   }
+const nextButtonObserver = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      if (mutation.attributeName === 'class') {
+        const disabled = nextButton.classList.contains('button-disabled');
+        if (disabled) {
+            nextButton.style.opacity = '0.5';
+            nextButton.style.cursor = 'not-allowed';
+        } else {
+            nextButton.style.opacity = '1';
+            nextButton.style.cursor = 'pointer';
+        }
+      }
+    });
+  });
+  
+  nextButtonObserver.observe(nextButton, { attributes: true });
 
-//   if (swiperProjects.isBeginning) {
-//     prevButton.classList.add('disabled-button');
-//   } else {
-//     prevButton.classList.remove('disabled-button');
-//   }
-// }
+  
+  const prevButtonObserver = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      if (mutation.attributeName === 'class') {
+        const disabled = prevButton.classList.contains('button-disabled');
+        if (disabled) {
+            prevButton.style.opacity = '0.5';
+            prevButton.style.cursor = 'not-allowed';
+        } else {
+            prevButton.style.opacity = '1';
+            prevButton.style.cursor = 'pointer';
+        }
+      }
+    });
+  });
+  
+  prevButtonObserver.observe(prevButton, { attributes: true });
 
-// updateButtonState();
+
+
 
  
 
-
-
-// const nextButtonObserver = new MutationObserver(mutations => {
-//   mutations.forEach(mutation => {
-//     if (mutation.attributeName === 'class') {
-//       const disabled = nextButton.classList.contains('button-disabled');
-//       if (disabled) {
-//           nextButton.style.opacity = '0.5';
-//           nextButton.style.cursor = 'not-allowed';
-//       } else {
-//           nextButton.style.opacity = '1';
-//           nextButton.style.cursor = 'pointer';
-//       }
-//     }
-//   });
-// });
-
-// const prevButtonObserver = new MutationObserver(mutations => {
-//   mutations.forEach(mutation => {
-//     if (mutation.attributeName === 'class') {
-//       const disabled = prevButton.classList.contains('button-disabled');
-//       if (disabled) {
-//           prevButton.style.opacity = '0.5';
-//           prevButton.style.cursor = 'not-allowed';
-//       } else {
-//           prevButton.style.opacity = '1';
-//           prevButton.style.cursor = 'pointer';
-//       }
-//     }
-//   });
-// });
-
-// nextButtonObserver.observe(nextButton, { attributes: true });
-// prevButtonObserver.observe(prevButton, { attributes: true });
