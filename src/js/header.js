9e419085fdee92062ca0c14981b5090ce342fdc0
AJ-Andrzej <aayaschenko@gmail.com>
2024-04-15 22:10:35 +0200

    update

--- src/js/header.js
+++ src/js/header.js
@@ -5,15 +5,18 @@
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
     menuLIst: document.querySelector('[data-menu-list]'),
+    orderBTN: document.querySelector('[data-orderBTN]')
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
   refs.menuLIst.addEventListener('click', toggleModal);
+  refs.orderBTN.addEventListener('click', toggleModal)
   function toggleModal() {
     refs.menuLIst.classList.remove('is-open');
     refs.menu.classList.toggle('is-open');
     refs.body.classList.toggle('no-scroll');
+    refs.orderBTN.classList.remove('is-open');
   }
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
