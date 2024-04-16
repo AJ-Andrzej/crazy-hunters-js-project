c4acd47b1462a6f55f9356a7b3a90f3069267e15
Dmytro1573 <124176235+Dmytro1573@users.noreply.github.com>
2024-04-16 19:47:12 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -5,21 +5,27 @@
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
     menuLIst: document.querySelector('[data-menu-list]'),
+    menuInput: document.querySelector('[data-menu-input]'),
+    menuItem: document.querySelector('[data-menu-item]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
   refs.menuLIst.addEventListener('click', toggleModal);
+  refs.menuItem.addEventListener('click', removeCheck);
   function toggleModal() {
     refs.menuLIst.classList.remove('is-open');
     refs.menu.classList.toggle('is-open');
     refs.body.classList.toggle('no-scroll');
   }
+
+  function removeCheck() {
+    refs.menuInput.checked = false;
+  }
+
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     refs.menu.classList.remove('is-open');
     refs.body.classList.remove('no-scroll');
   });
 })();
-
-
